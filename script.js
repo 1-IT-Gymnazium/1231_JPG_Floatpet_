00e6ceb4631b0cf9d8ae779e6620fc0d1cc3c8f2
Michael2838110 <1231@student.itgymnazium.cz>
2026-03-30 02:21:46 +0200

    Update floater size for added drawings
    Increased the size of the floater images added from drawings.

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ document.getElementById("addDrawing").addEventListener("click", () => {
     y: Math.random() * canvas.height,
     dx: (Math.random() - 0.5) * 4,
     dy: (Math.random() - 0.5) * 4,
-    size: canvas.width / 2
+    size: 300
   });
 
   dctx.clearRect(0, 0, drawCanvas.width, drawCanvas.height);
